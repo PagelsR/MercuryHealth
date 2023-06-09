674a9f8e6f86aae87811771221b0125076f4f448
Randy Pagels <rpagels@xpirit.com>
2023-06-09 13:09:53 -0400

    Updates

--- MercuryHealth.PlaywrightTests/Verify_Nutritions.spec.ts
+++ MercuryHealth.PlaywrightTests/Verify_Nutritions.spec.ts
@@ -14,3 +14,56 @@ test('should allow me to navigate to nutritions page', async ({ page }) => {
   await page.screenshot({ path: 'screenshot_Home-Nutritionspage.png', fullPage: true, timeout: 60000 });
 
 });
+
+
+test('Verify_NavToNutritionDetail', async () => {
+
+
+  const acceptPolicyButton = await page.$('#accept-policy close');
+  if (acceptPolicyButton !== null) {
+    await acceptPolicyButton.click();
+  }
+
+  const myPageTitle = await page.title();
+  expect(myPageTitle).toBe('Home Page - Mercury Health');
+
+  await page.click('#menu_nutrition');
+  const nutritionPageTitle = await page.title();
+  expect(nutritionPageTitle).toBe('Nutrition - Mercury Health');
+
+  await page.click('#button_details_25');
+  const detailsPageTitle = await page.title();
+  expect(detailsPageTitle).toBe('Details - Mercury Health');
+
+  await page.screenshot({
+    path: 'screenshot_nutrition_details_25.png',
+    fullPage: true
+  });
+
+  expect(page.url()).toBe(pageURL + 'Nutritions/Details/25');
+
+  let myDescription = await page.textContent('id=Description');
+  myDescription = myDescription.replace(/\n/g, '');
+  myDescription = myDescription.trim();
+
+  await page.screenshot({
+    path: 'screenshot_Item-Description.png',
+    fullPage: true
+  });
+
+  const rnd = Math.floor(Math.random() * 2) + 1;
+  if (rnd === 1) {
+    expect(myDescription).toBe('Banana');
+  } else {
+    expect(myDescription).toBe('Strawberry');
+  }
+
+  await page.click('text=Home');
+
+  await context.tracing.stop({
+    path: 'trace_Verify_NavToNutritionDetail.zip'
+  });
+
+  await browser.close();
+});
+
