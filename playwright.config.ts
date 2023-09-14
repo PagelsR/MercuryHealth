4552201e02e4b40eb4d285024d065a2a88cbdd6f
Randy Pagels <pagelsr@live.com>
2023-09-14 06:22:16 -0400

    Dev in EastUS2

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,8 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     /* on-first-retry or on*/
     trace: 'on-first-retry',
-    video: 'on-first-retry',
+    /* Collect Video when retrying the failed test. */
+    /* video: 'on-first-retry', */
   },
 
   /* Configure projects for major browsers */
