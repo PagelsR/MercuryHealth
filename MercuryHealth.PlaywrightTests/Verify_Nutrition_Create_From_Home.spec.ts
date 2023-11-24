cd758044086a53aa4da773bc8c35557fc5026be6
Randy Pagels <pagelsr@live.com>
2023-11-24 09:00:49 -0500

    New Tests

---
+++ MercuryHealth.PlaywrightTests/Verify_Nutrition_Create_From_Home.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+
+// Dynamicly set the URL from pipeline output
+test.beforeEach(async ({ page }) => {
+  const url = process.env.website_URL || 'https://app-okhgzqoexg6jy.azurewebsites.net/';
+  await page.goto(url , { waitUntil: 'load', timeout: 15000 });
+});
+
+test('Create a nutrition record from home page', async ({ page }) => {
+  await page.getByRole('link', { name: 'Create Nutrition' }).click();
+  await page.getByLabel('Description').click();
+  await page.getByLabel('Description').fill('Create New Record');
+  await page.getByLabel('Quantity').click();
+  await page.getByLabel('Quantity').fill('1');
+  await page.getByLabel('Meal Time').click();
+  await page.getByLabel('Meal Time').fill('2023-11-24T00:12');
+  await page.getByLabel('Tags').click();
+  await page.getByLabel('Tags').fill('New Record');
+  await page.getByLabel('Calories').click();
+  await page.getByLabel('Calories').fill('1');
+  await page.getByLabel('Protein/g').click();
+  await page.getByLabel('Protein/g').fill('1');
+  await page.getByLabel('Fat/g').click();
+  await page.getByLabel('Fat/g').fill('1');
+  await page.getByLabel('Carbohydrates/g').click();
+  await page.getByLabel('Carbohydrates/g').fill('1');
+  await page.getByLabel('Sodium/g').click();
+  await page.getByLabel('Sodium/g').fill('1');
+  await page.getByLabel('Color').click();
+  await page.getByLabel('Color').fill('Blue');
+  await page.getByRole('button', { name: 'Create' }).click();
+
+});
+
+test('Delete a nutrition record from home page', async ({ page }) => {
+  await page.getByRole('link', { name: 'Nutrition', exact: true }).click();
+  await expect(page).toHaveTitle('Nutrition - Mercury Health');
+
+  await page.locator('#button_delete_30').click();
+  const detailsPageTitle = await page.title();
+  expect(detailsPageTitle).toBe('Delete - Mercury Health');
+  await page.getByRole('button', { name: 'Delete' }).click();
+  await page.getByRole('link', { name: 'Home', exact: true }).click();
+
+});
--- MercuryHealth.PlaywrightTests/Verify_Nutritions.spec.ts
+++ MercuryHealth.PlaywrightTests/Verify_Nutritions.spec.ts
@@ -141,3 +141,32 @@ test('Navigate to nutritions page and verify details', async ({ page }) => {
   await page.click('#button_back');
 
 });
+
+test('Create an exercise record from home page', async ({ page }) => {
+  await page.getByRole('link', { name: 'Create Exercise' }).click();
+  await page.getByLabel('Name').click();
+  await page.getByLabel('Name').fill('Create New Record');
+  await page.getByLabel('Description').click();
+  await page.getByLabel('Description').fill('Create New Record');
+  await page.getByLabel('Exercise Time').click();
+  await page.getByLabel('Exercise Time').fill('2023-11-24T00:12');
+  await page.getByLabel('Muscles').click();
+  await page.getByLabel('Muscles').fill('NA');
+  await page.getByLabel('Equipment').click();
+  await page.getByLabel('Equipment').fill('NA');
+  await page.getByRole('button', { name: 'Create' }).click();
+
+});
+
+test('Delete an exercise record from home page', async ({ page }) => {
+  await page.getByRole('link', { name: 'Exercises', exact: true }).click();
+  await expect(page).toHaveTitle('Exercises - Mercury Health');
+
+  await page.click('#button_details_30');
+  const detailsPageTitle = await page.title();
+  expect(detailsPageTitle).toBe('Delete - Mercury Health');
+
+  await page.getByRole('button', { name: 'Delete' }).click();
+  await page.getByRole('link', { name: 'Home', exact: true }).click();
+
+});
